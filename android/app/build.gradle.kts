2765b8467854da86e5fe188b06e16c6204007765
toboclaw <tobomobo@users.noreply.github.com>
2026-08-27 16:38:11 +0200

    chore(android): prepare AirPods Max rc4

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,6 +1,6 @@
 import java.util.Properties
 
-val appVersionName = "1.0.0-rc3"
+val appVersionName = "1.0.0-rc4"
 
 plugins {
     alias(libs.plugins.android.application)
@@ -49,7 +49,7 @@ android {
     defaultConfig {
         applicationId = "me.kavishdevar.librepods"
         targetSdk = 37
-        versionCode = 64
+        versionCode = 65
         versionName = appVersionName
     }
     buildTypes {
